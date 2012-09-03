cfc54561921f8b0dd93b01752e6b4bfe10c40b66
Jakub Przybytek <cubanan@gmail.com>
2012-09-03 11:00:22 +0200

    Copyright year changed to 2012

--- src/main/java/com/cognifide/maven/plugins/crx/CrxPackageInstallMojo.java
+++ src/main/java/com/cognifide/maven/plugins/crx/CrxPackageInstallMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Cognifide Limited
+ * Copyright 2012 Cognifide Limited
  * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in
  * compliance with the License. You may obtain a copy of the License at
